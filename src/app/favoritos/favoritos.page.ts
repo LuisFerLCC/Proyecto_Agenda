7211d4b4ef7f911d371e3801da10a2710c9123aa
LuisFerLCC <luisfern837@gmail.com>
2024-11-28 16:36:26 -0600

    Interfaz Favoritos

--- src/app/favoritos/favoritos.page.ts
+++ src/app/favoritos/favoritos.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+import { Evento } from '../models/evento.model';
+
 import { IonicModule } from '../modules/ionic/ionic.module';
 
 @Component({
@@ -12,5 +14,38 @@ import { IonicModule } from '../modules/ionic/ionic.module';
   imports: [IonicModule, FormsModule, RouterModule],
 })
 export class FavoritosPage {
-  constructor() {}
+  eventos: Evento[] = [
+    {
+      id: 2,
+      titulo: 'Concierto de jazz',
+      descripcion: 'Concierto de jazz en el teatro de la ciudad',
+      categoria: 'Concierto',
+      fecha: new Date(2024, 11, 5, 13, 30),
+      direccion: 'Calle 456',
+      fotoUrl: 'https://picsum.photos/57',
+      esFavorito: true,
+    },
+  ];
+
+  formatearFecha(fecha: Date): string {
+    const fechaActual = new Date();
+
+    const esEsteAnio = fecha.getFullYear() === fechaActual.getFullYear();
+    const esEsteMes = esEsteAnio && fecha.getMonth() === fechaActual.getMonth();
+    const esEstaSemana =
+      esEsteMes &&
+      fecha.getDate() >= fechaActual.getDate() &&
+      fecha.getDate() < fechaActual.getDate() + 7;
+    const esHoy = esEsteMes && fecha.getDate() === fechaActual.getDate();
+
+    return fecha.toLocaleString('es-MX', {
+      weekday: esHoy ? undefined : 'long',
+      day: esEstaSemana ? undefined : 'numeric',
+      month: esEsteMes ? undefined : 'long',
+      year: esEsteAnio ? undefined : 'numeric',
+
+      hour: 'numeric',
+      minute: '2-digit',
+    });
+  }
 }
