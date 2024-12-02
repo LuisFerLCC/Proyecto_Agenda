5345c5f6d64ab58ca8483dc8356e00603fb515c8
LuisFerLCC <luisfern837@gmail.com>
2024-12-01 21:08:28 -0600

    Funcionalidad Detalles

--- src/app/detalles/detalles.page.ts
+++ src/app/detalles/detalles.page.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+
+import { Share } from '@capacitor/share';
+import { Platform } from '@ionic/angular';
+
+import { Evento } from '../models/evento.model';
 
 import { IonicModule } from '../modules/ionic/ionic.module';
+
+import { EventosService } from '../services/eventos.service';
 
 @Component({
   selector: 'app-detalles',
@@ -11,15 +19,55 @@ import { IonicModule } from '../modules/ionic/ionic.module';
   standalone: true,
   imports: [IonicModule, FormsModule, RouterModule],
 })
-export class DetallesPage {
+export class DetallesPage implements OnInit {
+  constructor(
+    public eventosService: EventosService,
+    private location: Location,
+    private platform: Platform,
+    private route: ActivatedRoute
+  ) {}
+
   botones = [
     { text: 'Cancelar', role: 'cancel', handler() {} },
     {
       text: 'Eliminar',
       role: 'delete',
-      handler() {
-        console.log('Eliminar');
+      handler: () => {
+        if (!this.evento) return;
+
+        this.eventosService.eliminarEvento(this.evento.id);
+        this.location.back();
       },
     },
   ];
+
+  evento?: Evento;
+  puedeCompartir = false;
+
+  async compartir() {
+    if (!this.evento) return;
+
+    await Share.share({
+      title: this.evento.titulo,
+      text: `¡No te pierdas mi evento!
+
+${this.evento.titulo}
+${this.evento.descripcion || '-'}
+
+Fecha: ${this.evento.fecha.toISOString().slice(0, 10)}
+Dirección: ${this.evento.direccion}`,
+      dialogTitle: 'Compartir evento',
+    });
+  }
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.eventosService
+      .obtenerEvento(parseInt(id ?? '0', 10))
+      .then((evento) => {
+        this.evento = evento;
+      });
+
+    this.puedeCompartir = this.platform.is('mobile');
+  }
 }
